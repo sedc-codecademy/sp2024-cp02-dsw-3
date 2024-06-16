c0a76383bc8966ae23f5f525b3195ca5229e5f1e
Ana Mihajlovska <michona@Ana.local>
2024-06-16 16:46:00 +0200

    update local storage

--- DropshippingStore/scripts/cart.js
+++ DropshippingStore/scripts/cart.js
@@ -1,39 +1,59 @@
+import LocalStorageService from "./localStorage.js";
 
-// function setToLocalStorage(key, value) {
-//     localStorage.setItem(key, JSON.stringify(value));
-// };
+const localSt = new LocalStorageService();
 
-function getFromLocalStorage(key){
-    const item = localStorage.getItem(key);
-    return item ? JSON.parse(item) : null;
-};
+// test
+const testIds = { ids: ["1", "2", "3"] };
+localSt.setToLocalStorage("local", JSON.stringify(testIds));
 
-async function fetchAndDisplayProducts() {
-    try {
-        const response = await fetch('../products.json');
-        const data = await response.json();
-        const products = data.products;
-        // setToLocalStorage("shooping-cart", products)
-        displayCart(products);
-        displayAmount(products);
-    } catch (error) {
-        console.error('Error fetching products:', error);
-    }
-}
-
-fetchAndDisplayProducts();
-
+const saleTest = { ids: ["4"] };
+localSt.setToLocalStorage("sale", JSON.stringify(saleTest));
 
 let cart = [];
 
+//FETCH AND DISPLAY
+
+async function fetchAndDisplayProducts() {
+  try {
+    let savedObj = localSt.getFromLocalStorage("local");
+    let savedSaleObj = localSt.getFromLocalStorage("sale");
+
+    let savedIds = savedObj ? JSON.parse(savedObj).ids : [];
+    let saleSavedIds = savedSaleObj ? JSON.parse(savedSaleObj).ids : [];
+
+    if (savedIds.length > 0 || saleSavedIds.length > 0) {
+      const response = await fetch("../products.json");
+      const data = await response.json();
+      let products = data.products;
+
+      cart = products.filter(
+        (product) =>
+          savedIds.includes(product.id.toString()) ||
+          saleSavedIds.includes(product.id.toString())
+      );
+      console.log(cart);
+      displayCart(cart);
+      displayAmount(cart);
+    } else {
+      displayCart([]);
+      displayAmount([]);
+    }
+  } catch (error) {
+    console.log("Error fetching products", error);
+  }
+}
+
+//DISPLAY CART
 
 function displayCart(cart) {
-    const cartItemsContainer = document.querySelector('.cart-items');
-    if (cart.length === 0) {
-        cartItemsContainer.innerHTML = '<p class= "emptyCart">Your cart is empty.</p>';
-    } else {
-        const cartHTML = cart.map((item, index) => {
-            return `
+  const cartItemsContainer = document.querySelector(".cart-items");
+  if (cart.length === 0) {
+    cartItemsContainer.innerHTML =
+      '<p class= "emptyCart">Your cart is empty.</p>';
+  } else {
+    const cartHTML = cart
+      .map((item, index) => {
+        return `
                 <div class="cart-item">
                     <img src="${item.imageUrl}" alt="${item.description}">
                     <div class="cart-item-info" class="cartItemInfo">
@@ -44,29 +64,30 @@ function displayCart(cart) {
                     <button type="button" class="delete-btn" data-index="${index}">X</button>
 
                 </div>`;
-        }).join('');
-        cartItemsContainer.innerHTML = cartHTML;
-    }
+      })
+      .join("");
+    cartItemsContainer.innerHTML = cartHTML;
+  }
 
-    cartItemsContainer.addEventListener('click', (event) => {
-        event.preventDefault();
-        if (event.target.classList.contains('delete-btn')) {
-            const index = parseInt(event.target.getAttribute('data-index'));
-            cart = cart.filter((item, idx) => idx !== index);
-            displayCart(cart);
-            displayAmount(cart)
-            
-        }
-    });
+  cartItemsContainer.addEventListener("click", (event) => {
+    event.preventDefault();
+    if (event.target.classList.contains("delete-btn")) {
+      const index = parseInt(event.target.getAttribute("data-index"));
+      cart = cart.filter((item, idx) => idx !== index);
+      displayCart(cart);
+      displayAmount(cart);
+    }
+  });
 }
 
-function displayAmount(cart) {
-    
-    const subtotal = cart.reduce((total, item) => total + item.price, 0);
-    const shipping = cart.length === 0 ? 0 : 5; 
-    const total = subtotal + shipping;
+//DISPLAY AMOUNT
 
-    const amountHTML = `
+function displayAmount(cart) {
+  const subtotal = cart.reduce((total, item) => total + item.price, 0);
+  const shipping = cart.length === 0 ? 0 : 5;
+  const total = subtotal + shipping;
+
+  const amountHTML = `
         <div class="summary-item">
             <span>Subtotal:</span>
             <span>$${subtotal.toFixed(2)}</span>
@@ -81,13 +102,8 @@ function displayAmount(cart) {
         </div>
     `;
 
-    const amountContainer = document.querySelector('.cart-items');
-    amountContainer.insertAdjacentHTML('beforeend', amountHTML);
+  const amountContainer = document.querySelector(".cart-items");
+  amountContainer.insertAdjacentHTML("beforeend", amountHTML);
 }
 
-
-
-
-
-
- 
+fetchAndDisplayProducts();
--- DropshippingStore/scripts/localStorage.js
+++ DropshippingStore/scripts/localStorage.js
@@ -1,14 +1,12 @@
 class LocalStorageService {
-    getFromLocalStorage(key) {
-        const item = localStorage.getItem(key);
-        return item ? JSON.parse(item) : null;
-    }
+  getFromLocalStorage(key) {
+    const item = localStorage.getItem(key);
+    return item ? JSON.parse(item) : null;
+  }
 
-    setToLocalStorage(key, value) {
-        localStorage.setItem(key, JSON.stringify(value));
-    }
-
+  setToLocalStorage(key, value) {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
 }
-
 
 export default LocalStorageService;
