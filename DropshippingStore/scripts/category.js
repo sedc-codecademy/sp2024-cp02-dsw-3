1101388a77e769223ba77b1a189dffcb713962b4
tamaranaj <najdovska18@gmail.com>
2024-06-17 19:57:26 +0200

    add features

--- DropshippingStore/scripts/category.js
+++ DropshippingStore/scripts/category.js
@@ -1,3 +1,5 @@
+import { LocalStorageService } from "./classLS.js"
+
 let fetchDataService = {
     getImg: async function(add){
         try {
@@ -11,43 +13,40 @@ let fetchDataService = {
         }
     }
 }
-
+let currentPage = 1
+let items = 12
 const createCardsService = {
-    divShowingCards: document.getElementById("cardContainer"),
+    divShowingCards : document.getElementById("cardContainer"),
+    pageNumber: document.getElementById("pagination"),
     cardsDefault: async function(add){
         const data = await fetchDataService.getImg(add)
-        this.createCards(data)
-        
+        console.log(data)
+        await this.createCards(data, items,currentPage)
         categoriesService.addAscEvent(data)
         categoriesService.addDescEvent(data)
         
     },
-    filterImages: async function(add, inputCategory){
-        const data = await fetchDataService.getImg(add)
-        console.log(data)
-        const filteredData = data.filter(item=> item.category === inputCategory )
-        this.createCards(filteredData)
-        const arrayToBeSorted = [...filteredData]
-        categoriesService.addAscEvent(arrayToBeSorted)
-        categoriesService.addDescEvent(arrayToBeSorted)
+    createCards: async function(images,numOfImagesPerPage, page){
+        this.divShowingCards.innerHTML = "";
+        page--;
+        let start = numOfImagesPerPage * page
+        let end = start + numOfImagesPerPage 
+        let pagginatedItems = images.slice(start, end)
+        for(let i = 0; i< pagginatedItems.length; i++){
 
-    },
-    createCards: function(data){
-        createCardsService.divShowingCards.innerHTML = " ";
-        for (let i = 0; i < data.length; i++) {
             this.divShowingCards.innerHTML += `
              <article class="card">
                 <img
                 class="card__background"
-                src=${data[i].imageUrl}
-                alt=${data[i].type}
+                src=${pagginatedItems[i].imageUrl}
+                alt=${pagginatedItems[i].type}
                 width="1920"
                 height="2193"/>
                     <div class="card__content | flow">
                         <div class="card__content--container | flow">
-                            <p class="card__description">${data[i].category}</p>
-                            <p class="img-price">${data[i].price}$</p>
-                            <button class="card__button" id=${data[i].id}>Add to cart</button>
+                            <p class="card__description">${pagginatedItems[i].category}</p>
+                            <p class="img-price">${pagginatedItems[i].price}$</p>
+                            <button class="card__button" id=${pagginatedItems[i].id}>Add to cart</button>
                         </div>
                         
                     </div>
@@ -57,10 +56,38 @@ const createCardsService = {
         }
         
         addToCartService.addEventsAddToCart()
-        const arrayToBeSorted = [...data]
+        const arrayToBeSorted = [...images]
         categoriesService.addAscEvent(arrayToBeSorted)
         categoriesService.addDescEvent(arrayToBeSorted)
+        this.setupPagination(images,this.pageNumber, items)
+
+
+    }, setupPagination(images, wrapper, numOfImagesPerPage){
+        wrapper.innerHTML = ""
+        let page_count = Math.ceil(images.length / numOfImagesPerPage)
+
+        for( let i = 1; i<page_count +1; i++){
+            let btn= this.paginationButton(i, images)
+            wrapper.appendChild(btn)
+        }
+    },
+    paginationButton: function(page, images){
+        let button = document.createElement('button')
+        button.innerText = page
+        if(currentPage == page){
+            button.classList.add("active")
+        }
+        button.addEventListener("click", function(){
+            currentPage = page
+            createCardsService.createCards(images, items, currentPage)
+            let current_btn = document.querySelector('.page-numbers button.active')
+            current_btn.classList.remove('active')
+            button.classList.add('active')
+            
+        })
+        return button
     }
+    
 }
 
 const imageFilterService = {
@@ -74,30 +101,6 @@ const imageFilterService = {
         imageFilterService.stockFilter.addEventListener("change", imageFilterService.filterImages)
     },
 
-    displayImages: (data) => {
-        console.log("display filtered images: ", data)
-        document.getElementById("cardContainer").innerHTML = "";
-        for (let i = 0; i < data.length; i++) {
-            document.getElementById("cardContainer").innerHTML += `
-             <article class="card">
-                <img
-                class="card__background"
-                src=${data[i].imageUrl}
-                alt=${data[i].type}
-                width="1920"
-                height="2193"/>
-                    <div class="card__content | flow">
-                        <div class="card__content--container | flow">
-                            <p class="card__description">${data[i].category}</p>
-                            <button class="card__button">Add to cart</button>
-                        </div>
-                        
-                    </div>
-            </article> 
-           
-            `;
-        }
-    },
 
     filterImages: async () => {
 
@@ -116,38 +119,25 @@ const imageFilterService = {
             filteredImages = filteredImages.filter(image => image.stock === true);
         }
 
-        imageFilterService.displayImages(filteredImages);
+        createCardsService.createCards(filteredImages,items,currentPage);
     }
 };
 
 
 const categoriesService = {
     categorySelect: document.getElementById("categorySelect"),
-    //submitBtn: document.getElementById("submit"),
-    //addSubmitEvent: function(){
-        // this.submitBtn.addEventListener("click",function(){
-        //     createCardsService.divShowingCards.innerHTML = ""
-        //     if(categoriesService.categorySelect.value === "default"){
-        //         createCardsService.cardsDefault(url)
-        //     }else{
-        //         console.log(categoriesService.categorySelect.value)
-        //         createCardsService.filterImages(url, categoriesService.categorySelect.value)
-        //     }
-        // })
-        
-    //},
     ascendingBtn: document.getElementById("lowToHigh"),
     descendingBtn: document.getElementById("highToLow"),
     addAscEvent: function(data){
         this.ascendingBtn.addEventListener("click", function(){
             const arraySorted = data.sort(function(a,b){return a.price-b.price});
-            createCardsService.createCards(arraySorted)
+            createCardsService.createCards(arraySorted,items,currentPage)
         })
     },
     addDescEvent: function(data){
         this.descendingBtn.addEventListener("click", function(){
             const arraySorted = data.sort(function(a,b){return b.price-a.price})
-            createCardsService.createCards(arraySorted)
+            createCardsService.createCards(arraySorted,items,currentPage)
         })
     }
     
@@ -157,20 +147,24 @@ const categoriesService = {
 
 const searchInputService = {
     searchInput: document.getElementById("searchInput"),
-      addSearchEvent: this.searchInput.addEventListener("keydown",async function(event){
-            if(event.code === 'Enter'){
-                const searchedItems= await searchInputService.searchDB(url)
-                searchInputService.searchInput.value = '';
-                if(searchedItems.length==0){
-                    const divNoItems = document.getElementById("noItemsFound")
-                    divNoItems.style.display = "block"
-                    setTimeout(()=>{divNoItems.style.display = "none"}, 4000)
-                    await createCardsService.cardsDefault(url)
-                }else{
-                  createCardsService.createCards(searchedItems)
+      addSearchEvent: function (){
+        if(this.searchInput){
+            searchInputService.searchInput.addEventListener("keydown",async function(event){
+                if(event.code === 'Enter'){
+                    const searchedItems= await searchInputService.searchDB(url)
+                    searchInputService.searchInput.value = '';
+                    if(searchedItems.length==0){
+                        const divNoItems = document.getElementById("noItemsFound")
+                        divNoItems.style.display = "block"
+                        setTimeout(()=>{divNoItems.style.display = "none"}, 4000)
+                        await createCardsService.cardsDefault(url)
+                    }else{
+                      createCardsService.createCards(searchedItems)
+                    }
                 }
-            }
-        }),
+            })
+        }
+        },
     searchDB: async function(URl){
         const data = await fetchDataService.getImg(URl)
         const result = data.filter(item=>{
@@ -182,29 +176,25 @@ const searchInputService = {
     }
   
 }
-
+const storage = new LocalStorageService()
 
 const addToCartService = {
+    imageID: {ids:[]},
     addToCartBtn : document.getElementsByClassName("card__button"),
     addEventsAddToCart: function (){
         for(let button of this.addToCartBtn){
             button.addEventListener("click", function(event){
                 event.preventDefault()
-                localStorageService.imageID.ids.push(button.getAttribute('id'))
-                localStorageService.addToLocalStorage()
-                
+                addToCartService.imageID.ids.push(button.getAttribute('id'))
+                storage.setToLocalStorage("imageID", addToCartService.imageID)
+                itemsInCart.cart.innerText = `${storage.itemsInCart("imageID")}`
+                button.disabled = true                
             })
         }
         
     }
 }
 
-const localStorageService = {
-    imageID: {ids:[]},
-    addToLocalStorage: function(){
-        localStorage.setItem("imageID", JSON.stringify(this.imageID))
-    }
-}
 
 const promptInputService = {
     prompt: document.getElementById("promptInput"),
@@ -215,26 +205,17 @@ const promptInputService = {
     }
 }
 
-//TO DO 
-// const popUpImagesService = {
-//     imgButtons : document.getElementsByClassName("card__button"),
-//     addEventsImgButtons: function (){
-//         for(let button of this.imgButtons){
-//             button.addEventListener("click", function(event){
-//                 event.preventDefault()
-//             console.log("clicked")
-//             })
-//         }
-        
-//     }
-// }
-
-
-
+const itemsInCart = {
+    cart: document.getElementById("cart-count"),
+    displayItems: function(){
+        this.cart.innerText = `${storage.itemsInCart("imageID")}`
+    }
+}
 
 const url = 'https://raw.githubusercontent.com/sedc-codecademy/sp2024-cp02-dsw-3/feature/T8/category-page/DropshippingStore/images.json'
 createCardsService.cardsDefault(url) //presenting all images
-//categoriesService.addSubmitEvent()
 promptInputService.addPromptEvent() //redirect to singUp page
 imageFilterService.listenToCategoryFilter();
 imageFilterService.listenToStockFilter();
+searchInputService.addSearchEvent()
+itemsInCart.displayItems()
---
+++ DropshippingStore/scripts/classLS.js
@@ -0,0 +1,26 @@
+class LocalStorageService {
+    getFromLocalStorage(key) {
+        const item = localStorage.getItem(key);
+        return item ? JSON.parse(item) : null;
+    }
+
+    setToLocalStorage(key, value) {
+        localStorage.setItem(key, JSON.stringify(value));
+    }
+
+    clearLocalStorage(){
+        localStorage.clear()
+    }
+
+    itemsInCart(key){
+        const res = this.getFromLocalStorage(key)
+        if(res){
+            return res.ids.length
+        }
+        
+    }
+
+}
+
+
+export { LocalStorageService};
