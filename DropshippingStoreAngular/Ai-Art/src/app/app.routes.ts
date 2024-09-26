26a3a97a79a20ba01f4cb967727e0012d528d334
Ana Mihajlovska <153089355+anamihajlovska@users.noreply.github.com>
2024-09-26 13:07:56 +0200

    Feature/am10/home page (#28)
    * added home page
    
    * update
    
    * footer update

--- DropshippingStoreAngular/Ai-Art/src/app/app.routes.ts
+++ DropshippingStoreAngular/Ai-Art/src/app/app.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
+import { AccountComponent } from './account/account.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { CategoriesComponent } from './categories/categories.component';
+import { GenerateComponent } from './generate/generate.component';
+import { ContactComponent } from './contact/contact.component';
+import { LoginComponent } from './login/login.component';
+import { CartComponent } from './cart/cart.component';
+import { RegisterComponent } from './register/register.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'categories', component: CategoriesComponent },
+  { path: 'generate-your-art', component: GenerateComponent },
+  { path: 'contact', component: ContactComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'cart', component: CartComponent },
+  {
+    path: 'account',
+    component: AccountComponent,
+  },
+];
