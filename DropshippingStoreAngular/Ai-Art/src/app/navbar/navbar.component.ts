b48dd2a0c6e7b820e5989b92010ee86297a3b8d6
anamihajlovska <anamihajlovskaa123@gmail.com>
2024-09-26 12:46:28 +0200

    added home page

---
+++ DropshippingStoreAngular/Ai-Art/src/app/navbar/navbar.component.ts
@@ -0,0 +1,35 @@
+import { Component, ViewChild } from '@angular/core';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatSidenav, MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { NgFor } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [
+    MatToolbarModule,
+    MatButtonModule,
+    MatIconModule,
+    MatMenuModule,
+    MatSidenavModule,
+    MatListModule,
+    RouterModule,
+    NgFor,
+  ],
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css'],
+})
+export class NavbarComponent {
+  @ViewChild('drawer') drawer: MatSidenav | undefined;
+
+  closeSidenav(): void {
+    if (this.drawer) {
+      this.drawer.close();
+    }
+  }
+}
