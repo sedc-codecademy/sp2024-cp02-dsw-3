19cd5f8a74d9615b1553bcc8fb78c31590a87fb1
Ana Mihajlovska <anamihajlovskaa123@gmail.com>
2024-10-10 13:16:05 +0200

    adding css

--- DropshippingStoreAngular/Ai-Art/src/app/account/change-user-info/change-user-info.component.ts
+++ DropshippingStoreAngular/Ai-Art/src/app/account/change-user-info/change-user-info.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Output, EventEmitter, signal } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component, OnInit, signal } from '@angular/core';
+import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, ReactiveFormsModule } from '@angular/forms';
 import { AccountService } from '../../services/account.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -13,10 +13,15 @@ import { UserInfo } from '../../types/user-info.interface';
   styleUrls: ['./change-user-info.component.css']
 })
 export class ChangeUserInfoComponent implements OnInit {
-  userInfo = signal<UserInfo | null>(null)
+  userInfo = signal<UserInfo | null>(null);
   userInfoForm: FormGroup;
+  successMessage: string | null = null; 
 
-  constructor(private fb: FormBuilder, private route: ActivatedRoute, private readonly accountService: AccountService) {
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private readonly accountService: AccountService
+  ) {
     this.userInfoForm = this.fb.group({
       firstName: ['', [Validators.maxLength(50)]],
       lastName: ['', [Validators.maxLength(50)]],
@@ -25,19 +30,15 @@ export class ChangeUserInfoComponent implements OnInit {
       confirmPassword: ['', [Validators.minLength(8)]],
       email: ['', [Validators.email]],
       cardNo: ['', [Validators.pattern(/^\d{16}$/)]],
-      expireDate: ['', [Validators.pattern(/^(0[1-9]|1[0-2])\/?([0-9]{2})$/)]],
-    });
+      expireDate: ['', [Validators.pattern(/^(0[1-9]|1[0-9])\/?([0-9]{2})$/)]],
+    }, { validators: this.passwordsMatchValidator });
   }
 
   ngOnInit(): void {
-    
-        // Fetch user info from the service
     this.accountService.getUserInfo().subscribe({
       next: (data) => {
         console.log('User info response:', data);
-        this.userInfo.set(data.userInfo); // Update the signal with the fetched data
-        
-        // Populate the form with user data
+        this.userInfo.set(data.userInfo);
         this.userInfoForm.patchValue(data.userInfo);
       },
       error: (error) => {
@@ -49,22 +50,36 @@ export class ChangeUserInfoComponent implements OnInit {
     });
   }
 
+  passwordsMatchValidator(group: AbstractControl): ValidationErrors | null {
+    const password = group.get('password')?.value;
+    const confirmPassword = group.get('confirmPassword')?.value;
+
+    if (password && confirmPassword && password !== confirmPassword) {
+      group.get('confirmPassword')?.setErrors({ passwordsMismatch: true });
+      return { passwordsMismatch: true };
+    } else {
+      return null;
+    }
+  }
+
   onSubmit() {
     if (this.userInfoForm.valid) {
       const updatedUserInfo = this.userInfoForm.value;
 
-      // Call a service method to update the user info
       this.accountService.updateUserInfo(updatedUserInfo).subscribe({
         next: () => {
           console.log('User info updated successfully');
+          this.successMessage = 'User info updated successfully!'; 
+          setTimeout(() => this.successMessage = null, 3000);
         },
         error: (error) => {
           console.error('Error updating user info:', error);
+          this.successMessage = null; 
         }
       });
     } else {
       console.error('Form is invalid');
+      this.successMessage = null; 
     }
   }
-  }
-
+}
