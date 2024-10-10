d89ff8f775675d3d6045d0bcc7b8fbf4d464ac09
anamihajlovska <anamihajlovskaa123@gmail.com>
2024-10-10 14:00:17 +0200

    visibility icon added

--- DropshippingStoreAngular/Ai-Art/src/app/account/change-user-info/change-user-info.component.ts
+++ DropshippingStoreAngular/Ai-Art/src/app/account/change-user-info/change-user-info.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit, signal } from '@angular/core';
-import { FormBuilder, FormGroup, Validators, AbstractControl, ValidationErrors, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators,
+  AbstractControl,
+  ValidationErrors,
+  ReactiveFormsModule,
+} from '@angular/forms';
 import { AccountService } from '../../services/account.service';
 import { ActivatedRoute } from '@angular/router';
 import { CommonModule } from '@angular/common';
@@ -10,28 +17,49 @@ import { UserInfo } from '../../types/user-info.interface';
   standalone: true,
   imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './change-user-info.component.html',
-  styleUrls: ['./change-user-info.component.css']
+  styleUrls: ['./change-user-info.component.css'],
 })
 export class ChangeUserInfoComponent implements OnInit {
   userInfo = signal<UserInfo | null>(null);
   userInfoForm: FormGroup;
-  successMessage: string | null = null; 
+  successMessage: string | null = null;
+  showPassword: boolean = false;
+  showConfirmPassword: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     private route: ActivatedRoute,
     private readonly accountService: AccountService
   ) {
-    this.userInfoForm = this.fb.group({
-      firstName: ['', [Validators.maxLength(50)]],
-      lastName: ['', [Validators.maxLength(50)]],
-      userName: ['', [Validators.minLength(3)]],
-      password: ['', [Validators.minLength(8)]],
-      confirmPassword: ['', [Validators.minLength(8)]],
-      email: ['', [Validators.email]],
-      cardNo: ['', [Validators.pattern(/^\d{16}$/)]],
-      expireDate: ['', [Validators.pattern(/^(0[1-9]|1[0-9])\/?([0-9]{2})$/)]],
-    }, { validators: this.passwordsMatchValidator });
+    this.userInfoForm = this.fb.group(
+      {
+        firstName: ['', [Validators.maxLength(50)]],
+        lastName: ['', [Validators.maxLength(50)]],
+        userName: ['', [Validators.minLength(3)]],
+        password: ['', [Validators.minLength(8)]],
+        confirmPassword: ['', [Validators.minLength(8)]],
+        email: [
+          '',
+          [
+            Validators.email,
+            Validators.pattern(/^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/),
+          ],
+        ],
+        cardNo: [
+          '',
+          [
+            Validators.minLength(16),
+            Validators.maxLength(16),
+            Validators.pattern(/^[0-9]\d*$/),
+          ],
+        ],
+        expireDate: [
+          '',
+          [Validators.pattern(/^(0[1-9]|1[0-9])\/?([0-9]{2})$/)],
+        ],
+      },
+      { validators: this.passwordsMatchValidator }
+    );
   }
 
   ngOnInit(): void {
@@ -46,7 +74,7 @@ export class ChangeUserInfoComponent implements OnInit {
       },
       complete: () => {
         console.log('User info fetch complete');
-      }
+      },
     });
   }
 
@@ -62,6 +90,14 @@ export class ChangeUserInfoComponent implements OnInit {
     }
   }
 
+  togglePasswordVisibility(field: 'password' | 'confirmPassword') {
+    if (field === 'password') {
+      this.showPassword = !this.showPassword;
+    } else if (field === 'confirmPassword') {
+      this.showConfirmPassword = !this.showConfirmPassword;
+    }
+  }
+
   onSubmit() {
     if (this.userInfoForm.valid) {
       const updatedUserInfo = this.userInfoForm.value;
@@ -69,17 +105,17 @@ export class ChangeUserInfoComponent implements OnInit {
       this.accountService.updateUserInfo(updatedUserInfo).subscribe({
         next: () => {
           console.log('User info updated successfully');
-          this.successMessage = 'User info updated successfully!'; 
-          setTimeout(() => this.successMessage = null, 3000);
+          this.successMessage = 'User info updated successfully!';
+          setTimeout(() => (this.successMessage = null), 3000);
         },
         error: (error) => {
           console.error('Error updating user info:', error);
-          this.successMessage = null; 
-        }
+          this.successMessage = null;
+        },
       });
     } else {
       console.error('Form is invalid');
-      this.successMessage = null; 
+      this.successMessage = null;
     }
   }
 }
