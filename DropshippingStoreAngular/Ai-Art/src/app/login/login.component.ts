ed1ff1ffee4bb8a8ac5e64c51c5e4406d27822cb
anamihajlovska <anamihajlovskaa123@gmail.com>
2024-10-12 20:47:32 +0200

    css added

--- DropshippingStoreAngular/Ai-Art/src/app/login/login.component.ts
+++ DropshippingStoreAngular/Ai-Art/src/app/login/login.component.ts
@@ -1,51 +1,70 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+  NgModel,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { RouterLink, Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule, RouterLink],
+  imports: [
+    MatFormFieldModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    RouterLink,
+    CommonModule,
+  ],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.css'
+  styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  isPasswordVisible: boolean = false;
   [x: string]: any;
 
-  loginForm: FormGroup
-  
-  constructor (public readonly authService:AuthService, private readonly router: Router) {}
-  
-  ngOnInit () {
+  loginForm: FormGroup;
+
+  constructor(
+    public readonly authService: AuthService,
+    private readonly router: Router
+  ) {}
+
+  ngOnInit() {
     this.initForm();
   }
-  
-  initForm () {
+
+  initForm() {
     this.loginForm = new FormGroup({
       userName: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required])
-    })
-  };
-  
-  
-  submit () {
-    console.log(this.loginForm.value)
-    const { userName, password } = this.loginForm.value
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible;
+  }
+
+  submit() {
+    console.log(this.loginForm.value);
+    const { userName, password } = this.loginForm.value;
     this.authService.login(userName, password).subscribe((response) => {
       console.log(response);
-      if(response){
-        this.router.navigate(['/'])
-      };
+      if (response) {
+        this.router.navigate(['/']);
+      }
     });
-  };
-  
-  submitLogout(){
-    this.authService.logout()
-  };
   }
-  
 
+  submitLogout() {
+    this.authService.logout();
+  }
+}
--- DropshippingStoreAngular/Ai-Art/src/app/register/register.component.ts
+++ DropshippingStoreAngular/Ai-Art/src/app/register/register.component.ts
@@ -1,32 +1,40 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule, FormGroup, FormControl, Validators } from '@angular/forms';
+import {
+  ReactiveFormsModule,
+  FormGroup,
+  FormControl,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, ReactiveFormsModule, MatButtonModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.css',
 })
 export class RegisterComponent {
-  registerForm: FormGroup
+  registerForm: FormGroup;
   passwordMissMatch: boolean = false;
+  isPasswordVisible = false;
+  isConfirmPasswordVisible = false;
 
   constructor(
     private readonly authService: AuthService,
     private readonly router: Router
-  ){}
+  ) {}
 
-  ngOnInit () {
+  ngOnInit() {
     this.initForm();
   }
   //da se proverat validatorite
-  initForm () {
+  initForm() {
     this.registerForm = new FormGroup({
       firstName: new FormControl('', [Validators.required]),
       lastName: new FormControl('', [Validators.required]),
@@ -35,34 +43,59 @@ export class RegisterComponent {
       cardNo: new FormControl('', [Validators.required]),
       expireDate: new FormControl('', [Validators.required]),
       password: new FormControl('', [Validators.required]),
-      confirmPassword: new FormControl('', [Validators.required])
-    })
-  };
-  
-  submit () {
-//proverka na password-ot pred da se pusti request do backend
+      confirmPassword: new FormControl('', [Validators.required]),
+    });
+  }
+
+  submit() {
+    //proverka na password-ot pred da se pusti request do backend
     const registerPassword = this.registerForm.get('password')?.value;
     const confirmedPassword = this.registerForm.get('confirmPassword')?.value;
 
-    if(registerPassword !== confirmedPassword){
-      console.log('Passwords do not match')
+    if (registerPassword !== confirmedPassword) {
+      console.log('Passwords do not match');
       this.passwordMissMatch = true;
       return;
-    } else{
-      this.passwordMissMatch = false
-    };
-    
-// ako e vo red se registrira i prakja register do backend
-
-    console.log(this.registerForm.value)
-  const { firstName, lastName, userName, email, cardNo, expireDate, password } = this.registerForm.value
-   this.authService.register(firstName, lastName, userName, email, cardNo, expireDate, password).subscribe((response)=> {
-    console.log('Response from register', response);
-
-    if(response){
-      this.router.navigate(['/login'])
+    } else {
+      this.passwordMissMatch = false;
     }
-   })
-  };
-  
+
+    // ako e vo red se registrira i prakja register do backend
+
+    console.log(this.registerForm.value);
+    const {
+      firstName,
+      lastName,
+      userName,
+      email,
+      cardNo,
+      expireDate,
+      password,
+    } = this.registerForm.value;
+    this.authService
+      .register(
+        firstName,
+        lastName,
+        userName,
+        email,
+        cardNo,
+        expireDate,
+        password
+      )
+      .subscribe((response) => {
+        console.log('Response from register', response);
+
+        if (response) {
+          this.router.navigate(['/login']);
+        }
+      });
+  }
+
+  togglePasswordVisibility() {
+    this.isPasswordVisible = !this.isPasswordVisible; // Toggle visibility
+  }
+
+  toggleConfirmPasswordVisibility() {
+    this.isConfirmPasswordVisible = !this.isConfirmPasswordVisible; // Toggle visibility
+  }
 }
