b3c47f8ac8ff9a6c2390489755862e69605a6baf
tamaranaj <najdovska18@gmail.com>
2024-10-19 15:54:00 +0200

    update

--- DropshippingStoreAngular/Ai-Art/src/environment.ts
+++ DropshippingStoreAngular/Ai-Art/src/environment.ts
@@ -4,5 +4,5 @@ export const environment={
     API_IMAGES: 'http://localhost:5095/api/ImageContoller/'
 }
 
-export const STABILITY_KEY = 'sk-PfWa3u3lLhFD0yU7Zf3d41DqsS0zyY9cWP4rQPXheB7EzQBO'
+export const STABILITY_KEY = ''
 export const GENERATOR_API = 'https://api.stability.ai/v2beta/stable-image/generate/ultra';
